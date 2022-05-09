8573c50cbdf3937daefa86de4cd97ce55abf5e91
Yordan Pavlov <ThuF@users.noreply.github.com>
2022-05-09 13:46:18 +0300

    Create xsk.js

---
+++ ide-file-types/extensions/xsk.js
@@ -0,0 +1,23 @@
+exports.getFileTypes = function () {
+    return {
+        ".xsjs": "javascript",
+        ".xsjslib": "javascript",
+        ".hdbsynonym": "json",
+        ".hdbpublicsynonym": "json",
+        ".hdi": "json",
+        ".hdiconfig": "json",
+        ".hdbcalculationview": "xml",
+        ".hdbtable": "sql",
+        ".hdbview": "sql",
+        ".hdbprocedure": "sql",
+        ".hdbtablefunction": "sql",
+        ".hdbschema": "sql",
+        ".hdbdd": "sql",
+        ".hdbflowgraph": "xml",
+        ".xsodata": "sql",
+        ".xsaccess": "json",
+        ".xsprivileges": "json",
+        ".csvim": "json",
+        ".calculationview": "xml"
+    }
+};
